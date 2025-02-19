dadf1462710c6e331cf403a061e7141ec762ad67
Aldair Alejandro González Araujo <aldairalejandrogonzalezaraujo@gmail.com>
2025-02-18 23:59:50 -0600

    Se trabajo Publications

---
+++ src/publications/publication.controller.js
@@ -0,0 +1,148 @@
+import { response, request } from 'express';
+import Publication from './publication.model.js';
+import User from '../users/user.model.js';
+
+export const savePublication = async (req, res) =>{
+    try {
+        const data = req.body;
+        const user = req.usuario;
+
+        const publication = await Publication.create({
+            title: data.name,
+            category: data.category,
+            mainText: data.mainText,
+            publisher: user.id
+        })
+
+        await User.findByIdAndUpdate(user._id, {
+            $push: {publications: publication._id}
+        })
+
+        return res.status(200).json({
+            msg: 'Publication registered successfully!',
+            publicationDetails:{
+                publication: publication.title
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            msg:'Error to save the publication',
+            error: error.message
+        })
+    }
+}
+
+export const getPublication = async (req = request, res = response) => {
+    try {
+        const { limite = 10, desde = 0} = req.query;
+        const query = {status: true};
+
+        const [total, publications] = await Promise.all([
+            Publication.countDocuments(query),
+            Publication.find(query)
+                .skip(Number(desde))
+                .limit(Number(limite))
+        ]);
+
+        res.status(200).json({
+            success: true,
+            total, 
+            publications
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            msg:'Erro to get the publications',
+            error: error.message
+        })
+    }
+}
+
+export const searchPublication = async (req, res) =>{
+    try {
+        const { id } = req.params;
+        const publication = await Publication.findById(id);
+
+        if(!publication){
+            return res.status(404).json({
+                success: false,
+                msg:'Publication not found'
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            publication
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: true,
+            msg: 'Error to search the publication',
+            error: error.message
+        })
+    }
+}
+
+export const updatePublication = async (req, res = response) =>{
+    try {
+        const { id } = req.params;
+        const user = await Publication.findById(id).select('publisher'); 
+        const autheticatedUser = req.usuario;
+        const {_id, publisher, ...data} = req.body
+        
+        if(autheticatedUser._id.toString() === user.id){
+            const publication = await Publication.findByIdAndUpdate(id, data, {new: true});
+            return res.status(200).json({
+                success: true,
+                msg: 'Publication update successfully!',
+                publication
+            })
+        }
+
+        return res.status(403).json({
+            success: false,
+            msg: 'Only the user can edit their post'
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            msg:'Error to update the publication',
+            error: error.message
+        })
+    }
+}
+
+export const deletePublication = async (req, res) => {
+    try {
+            const { id } = req.params;
+            const user = await Publication.findById(id).select('publisher');
+            const autheticatedUser = req.usuario;
+
+            if(autheticatedUser._id.toString() === user.publisher.toString()){
+                const publication = await Publication.findByIdAndUpdate(id, {status:false}, {new: true});
+                await User.findByIdAndUpdate(publication.publisher, {
+                    $pull: {publications: updatePublication._id}
+                });
+                
+                return res.status(200).json({
+                    success: true,
+                    msg:'Publication deleted successfully',
+                    publication
+                })
+                
+            }
+
+            return res.status(403).json({
+                success: false,
+                msg:'Only the user can delete their post'
+            })
+            
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            msg: 'Error to delete the publication',
+            error: error.message
+        })
+    }
+}
---
+++ src/publications/publication.model.js
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+
+const PublicationSchema = Schema({
+    title:{
+        type: String,
+        required: [true, 'Title is required'],
+        maxLength: [25, 'Cant be overcome 25 characters']
+    },
+    category:{
+        type: String,
+        required: [true, 'Category is required'],
+    },
+    mainText:{
+        type: String,
+        required: [true, 'The post cannot be empty ']
+    },
+    status:{
+        type: Boolean,
+        default: true
+    },
+    publisher:{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'The publication need a publisher']
+    },
+    comments:[{
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+    }],
+
+}, 
+{
+    timesStamp:true,
+    versionKey: false
+}
+);
+
+export default model('Publication', PublicationSchema);
